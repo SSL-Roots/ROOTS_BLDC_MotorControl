e0b876d0a2ab3d3f0ed92c6558604bdc5ef06017
Tomoaki Yoshida <emitter22@gmail.com>
2017-05-03 16:36:08 +0900

    プロジェクト化

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ECM.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ECM.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ecm.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ecm.x.tar
